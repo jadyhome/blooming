fb1d834557bc748314ad16155028b465798ecbfd
Jady Hom <hyjady@gmail.com>
2020-10-22 18:47:55 -0400

    homepage is done

--- homepg.js
+++ homepg.js
@@ -1,10 +1,16 @@
-let current = 0
+let currentPhotos = 0
 let slides = document.getElementsByTagName('img')
 
-setInterval(function() {
+setInterval(function () {
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.opacity = 0
     }
-    current = (current != slides.length - 1) ? current + 1 : 0
-    slides[current].style.opacity = 2
-}, 3000)
+    if (currentPhotos != slides.length - 1) {
+        currentPhotos = currentPhotos + 1
+    }
+    else {
+        currentPhotos = currentPhotos = 0
+    }
+    slides[currentPhotos].style.opacity = 2
+}, 2500)
+setInterval()
